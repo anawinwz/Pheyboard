a0449a4e611ee91ea69e24718443b4338dab446f
Torrmai <coverleaf@localhost.localdomain>
2019-10-24 16:53:48 +0700

    finished settting bar

--- android/components/circle.js
+++ android/components/circle.js
@@ -1,6 +1,6 @@
 //will change to touchable content in latter
 import React, { Component } from 'react';
-import { StyleSheet, View,TouchableHighlight,Image} from 'react-native';
+import { StyleSheet, View,TouchableHighlight,Image,Text} from 'react-native';
 export default class SemiCircle extends Component{
     state = {collapse:true};
     checkCollapse =() =>{
@@ -11,16 +11,28 @@ export default class SemiCircle extends Component{
         return(
             <View style={styles.circle_wrapper}>
               {this.state.collapse ? 
-                <TouchableHighlight onPress={this.checkCollapse} style={styles.circle}>
-                  <Image style={{width:75,height:75}} source={require('../assets/index.png')}/>
+                <TouchableHighlight onPress={this.checkCollapse}  style={styles.circle}>
+                  <Image style={{width:75,height:75}} source={require('../assets/option.png')}/>
                 </TouchableHighlight>:
                 <View style={styles.circle_full}>
-
+                  <TouchableHighlight onPress={this.checkCollapse} style={styles.setting_button}>
+                    <Image style={{width:50,height:50}} source={require('../assets/right.png')}/>                    
+                  </TouchableHighlight>
+                  <TouchableHighlight onPress={this.checkCollapse} style={styles.setting_button}>                  
+                    <Image style={{width:50,height:50}} source={require('../assets/add.png')}/>                                      
+                  </TouchableHighlight>
+                  <TouchableHighlight onPress={this.checkCollapse} style={styles.setting_button}>
+                    <Image style={{width:50,height:50}} source={require('../assets/delete.png')}/>                    
+                  </TouchableHighlight>
+                  <TouchableHighlight onPress={this.checkCollapse} style={styles.setting_button}>
+                    <Image style={{width:50,height:50}} source={require('../assets/sets.png')}/>                    
+                  </TouchableHighlight>                  
                 </View>}
             </View>
         )
     }
 }
+const size_of_box = 50
 const styles = StyleSheet.create({
     circle_wrapper:{
       flexDirection:"row-reverse",
@@ -28,6 +40,8 @@ const styles = StyleSheet.create({
     },
     circle_full:{
       flexDirection:"row",
+      justifyContent:"space-evenly",
+      alignItems:"center",
       width:300,
       height:100,
       borderBottomLeftRadius:200,
@@ -42,5 +56,14 @@ const styles = StyleSheet.create({
       borderBottomLeftRadius:200,
       borderTopLeftRadius:200,
       backgroundColor:"white"
+    },
+    setting_button:{
+      justifyContent:"center",
+      alignItems:"center",
+      width:size_of_box,     
+      height:size_of_box,    
+      borderRadius:10,// change how round the box
+      display: "flex",
+      margin: 5         
     }
   })
