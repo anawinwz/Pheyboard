2b173bab846778f642ee41ff7992e5a5c4ffcfc4
Anawin W <imonmac@hotmail.com>
2019-11-05 20:22:35 +0700

    add BluetoothUtilPackage

--- android/android/app/src/main/java/com/pheyboard/MainApplication.java
+++ android/android/app/src/main/java/com/pheyboard/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.pheyboard.CheckerPackage;
+import com.pheyboard.BluetoothUtilPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
